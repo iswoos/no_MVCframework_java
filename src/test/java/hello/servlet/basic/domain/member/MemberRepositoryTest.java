f1295a7b826ed4bcb78c18bb7b7a35cbbea4bda7
leeseungwoo <vugil@naver.com>
2022-12-20 11:22:43 +0900

    domain 테스트 전체 완료

--- src/test/java/hello/servlet/basic/domain/member/MemberRepositoryTest.java
+++ src/test/java/hello/servlet/basic/domain/member/MemberRepositoryTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberRepositoryTest {
@@ -25,32 +28,24 @@ class MemberRepositoryTest {
 
         //then
         Member findMember = memberRepository.findbyId(saveMember.getId());
-        Assertions.assertThat(findMember).isEqualTo(saveMember);
-    }
-
-    @Test
-    void findbyId() {
-        //given
-
-
-        //when
-
-
-        //then
-
-
+        assertThat(findMember).isEqualTo(saveMember);
     }
 
     @Test
     void findAll() {
         //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 21);
 
+        memberRepository.save(member1);
+        memberRepository.save(member2);
 
         //when
-
+        List<Member> result = memberRepository.findAll();
 
         //then
-
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
 
     }
 
