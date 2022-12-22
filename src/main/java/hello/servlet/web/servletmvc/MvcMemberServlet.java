560db10a0b016e97f05051a71b56b861079dc36d
leeseungwoo <vugil@naver.com>
2022-12-22 10:55:30 +0900

    controller -> model -> view 처리 참고내용

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberServlet.java
@@ -11,6 +11,16 @@ import java.io.IOException;
 @WebServlet(name = "mvcMemberServlet", urlPatterns = "/servlet-mvc/members/new-form")
 public class MvcMemberServlet extends HttpServlet {
 
+/*
+/WEB-INF
+> 이 경로안에 JSP가 있으면 외부에서 직접 JSP를 호출할 수 없다. 우리가 기대하는 것은 항상 컨트롤러를
+    통해서 JSP를 호출하는 것이다.
+
+> redirect vs forward
+> 리다이렉트는 실제 클라이언트(웹 브라우저)에 응답이 나갔다가, 클라이언트가 redirect 경로로 다시
+    요청한다. 따라서 클라이언트가 인지할 수 있고, URL 경로도 실제로 변경된다. 반면에 포워드는 서버
+    내부에서 일어나는 호출이기 때문에 클라이언트가 전혀 인지하지 못한다
+*/
 
     /*
     WEB_INF 폴더에 있는 파일들은 외부에서 호출해도 그냥 호출되지 않는다. 항상 컨트롤러(서블릿)을 거쳐서
